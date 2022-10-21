97decbe35f5a996ed10fe0bdf71c44e04c96f52a
HannahPetry <48458@novasbe.pt>
2022-10-21 15:41:21 +0100

    add classification output to streamlit

---
+++ src/streamlit/helper.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+"""
+Description:
+    Predicting probability for medical labels from saved model 
+    
+Usage:
+    
+Possible arguments:
+    * 
+"""
+
+import imblearn
+import pickle
+import pandas as pd
+import numpy as np
+import os
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def load_model(file_path: str) -> imblearn.pipeline.Pipeline:
+    """
+    Load model from disk
+    Parameters
+    ----------
+    model_name : str
+        path to saved model
+    Returns
+    -------
+    imblearn.pipeline.Pipeline
+        best model from train.py
+    """
+    model = pickle.load(open(file_path, "rb"))
+    return model
+
+
+def predict_probability(model: imblearn.pipeline.Pipeline, value: str) -> pd.DataFrame:
+    """
+    get probabilities for sample
+    Parameters
+    ----------
+    model : imblearn.pipeline.Pipeline
+        best model from train.py
+    value : str
+        sample
+    category_list: list[str]
+        list of unique labels
+    Returns
+    -------
+    pd.DataFrame
+        Probabilities for labels
+    """
+
+    prob_array = model.predict_proba(value)
+    prob_df = (
+        pd.DataFrame(prob_array, index=["Probability"], columns=model.classes_)
+        .transpose()
+        .sort_values(by="Probability", ascending=False)
+    )
+    return prob_df
+
+
+def main():
+    # Load model
+    file_path = os.path.join("models", "sklearn_logistic_regression_model.pkl")
+    model = load_model(file_path)
+
+    # Predict probability
+    to_pred = "coronary nitroglycerin muscle heart breast oxygen valve artery"
+    res_df = predict_probability(model, [to_pred])
+    print(res_df)
+
+
+if __name__ == "__main__":
+    main()
--- src/streamlit/streamlit_test_01.py
+++ src/streamlit/streamlit_test_01.py
@@ -1,10 +1,24 @@
 # -*- coding: utf-8 -*-
 import streamlit as st
 
+from helper import *
+
+# importing all the helper fxn from helper.py which we will create later
+
+import os
+
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+# from helper file we are importing load_model and predict_probability
+file_path = os.path.join("models", "sklearn_logistic_regression_model.pkl")
+model = load_model(file_path)
+
 st.title("Welcome to medical symptom checker")
 
-tab1, tab2, tab3 = st.tabs(
-    ["Speech to text", "Transcription", "Classification"]
+tab1, tab2, tab3, tab4 = st.tabs(
+    ["Speech to text", "Transcription", "Classification", "Probabilities"]
 )
 
 with tab1:
@@ -12,9 +26,7 @@ with tab1:
 
 with tab2:
     with st.form("inputfield", clear_on_submit=True):
-        transcription = st.text_area(
-            "Please describe how you are feeling"
-        )
+        transcription = st.text_area("Please describe how you are feeling")
 
         submit = st.form_submit_button("Submit")
         if submit:
@@ -27,24 +39,43 @@ with tab2:
                 "valve",
             ]
             st.write("These are the most important keywords")
-            st.write(keywords)
+            st.write(transcription)
 
 
 with tab3:
-    st.subheader(
-        "Based on our algroithm you should consider contacting these departments"
-    )
-    with st.expander(
-        "1. Cardiovascular / Pulmonary (Click for more information)"
-    ):
-        st.metric(label="Percentage of probability", value="70%")
-        st.write("Decision was based on theses keywords:")
-        st.write(["cardiac", "valve"])
+    prediction = predict_probability(model, [transcription])
+    prob1 = prediction["Probability"][0]
+    prob2 = prediction["Probability"][1]
+    prob3 = prediction["Probability"][2]
 
-    with st.expander("2. Surgery (Click for more information)"):
-        st.metric(label="Percentage of probability", value="30%")
-        st.write("Decision was based on theses keywords:")
-        st.write(["ventricular", "mitral"])
+    st.subheader(
+        "Based on our algorithm you should consider contacting these departments"
+    )
+    with st.expander(prediction.index[0]):
+        st.metric(label="Percentage of probability", value="{:.0%}".format(prob1))
+        st.write("Decision was based on these symptoms from your description:")
+        st.write("coming soon")
+        st.write("Most relevant symptoms for this department in general:")
+        st.write("coming soon")
+
+    with st.expander(prediction.index[1]):
+        st.metric(label="Percentage of probability", value="{:.0%}".format(prob2))
+        st.write("Decision was based on these symptoms from your description:")
+        st.write("coming soon")
+        st.write("Most relevant symptoms for this department in general:")
+        st.write("coming soon")
+
+    with st.expander(prediction.index[2]):
+        st.metric(label="Percentage of probability", value="{:.0%}".format(prob3))
+        st.write("Decision was based on these symptoms from your description:")
+        st.write("coming soon")
+        st.write("Most relevant symptoms for this department in general:")
+        st.write("coming soon")
+
+with tab4:
+    prediction = predict_probability(model, [transcription])
+    st.subheader("Probability of each department")
+    st.write(prediction)
 
 # Transcipription
 # DESCRIPTION:,1.  Normal cardiac chambers size.,2.  Normal left ventricular size.,3.  Normal LV systolic function.  Ejection fraction estimated around 60%.,4.  Aortic valve seen with good motion.,5.  Mitral valve seen with good motion.,6.  Tricuspid valve seen with good motion.,7.  No pericardial effusion or intracardiac masses.,DOPPLER:,1.  Trace mitral regurgitation.,2.  Trace tricuspid regurgitation.,IMPRESSION:,1.  Normal LV systolic function.,2.  Ejection fraction estimated around 60%.
