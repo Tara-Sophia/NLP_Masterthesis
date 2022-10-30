ef0f4e7bbd87f7b6916062dcd361fc884042c043
Tara-Sophia Tumbrägel <tara.tumbraegel@gmail.com>
2022-10-30 18:40:53 +0000

    Updating training file

--- src/nlp/training.py
+++ src/nlp/training.py
@@ -26,9 +26,9 @@ checkpoint = "emilyalsentzer/Bio_ClinicalBERT"  # "bert-base-cased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
 
-def tokenize_function(examples):
+def tokenize_function(batch):
     return tokenizer(
-        examples["transcription"], padding="max_length", truncation=True, max_length=512
+        batch["transcription"], padding="max_length", truncation=True, max_length=512
     )
 
 
@@ -97,45 +97,12 @@ def training_model(tokenized_dataset):
     return model
 
 
-from keybert import KeyBERT
-
-
-def KeywordExtraction(model, text):
-    model = KeyBERT(model_path="./results_modelBert")
-    keywords = model.extract_keywords(
-        text,
-        keyphrase_ngram_range=(1, 2),
-        stop_words="english",
-        use_maxsum=True,
-        nr_candidates=25,
-        top_n=10,
-        use_mmr=True,
-    )
-    return keywords
-
-
-def apply_keyword_on_Dataframe(model, df):
-    df["keywords_outcome"] = df["transcription"].apply(
-        lambda x: KeywordExtraction(model, x)
-    )
-    return df
-
-
-def save_dataframe(df):
-    df.to_csv("../data/raw/mtsamples_outcome_bert.csv", index=False)
-
-
-
-
 def main():
     data_path = "../data/raw/mtsamples_cleaned.csv"
     dataset = load_dataset(data_path)
     tokenized_dataset = tokenize_dataset(dataset)
     tokenized_dataset = clean_remove_column(tokenized_dataset)
     model = training_model(tokenized_dataset)
-    df = pd.read_csv(data_path)
-    df_outcome = apply_keyword_on_Dataframe(model, df)
-    save_dataframe(df_outcome)
 
 
 # Path: src/Keyword_Bert_Training.py
