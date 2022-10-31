90ffaa52f08fbcddefcd1316711e1a8e11806147
Tara-Sophia Tumbrägel <tara.tumbraegel@gmail.com>
2022-10-31 16:10:14 +0000

    constants smaller batch size

--- src/nlp/constants.py
+++ src/nlp/constants.py
@@ -15,9 +15,7 @@ MODEL_UNSUPERVISED_NAME = "emilyalsentzer/Bio_ClinicalBERT"
 MODEL_UNSUPERVISED_CHECKPOINTS_DIR = os.path.join(
     "models", "nlp", "unsupervised", "checkpoints"
 )
-MODEL_UNSUPERVISED_MODEL_DIR = os.path.join(
-    "models", "nlp", "unsupervised", "model"
-)
+MODEL_UNSUPERVISED_MODEL_DIR = os.path.join("models", "nlp", "unsupervised", "model")
 
 # MODEL_UNSUPERVISED
 
@@ -28,8 +26,8 @@ SEED_SPLIT = 0
 SEED_TRAIN = 0
 
 MAX_SEQ_LEN = 128
-TRAIN_BATCH_SIZE = 16
-EVAL_BATCH_SIZE = 16
+TRAIN_BATCH_SIZE = 8
+EVAL_BATCH_SIZE = 8
 LEARNING_RATE = 2e-5
 LR_WARMUP_STEPS = 100
 WEIGHT_DECAY = 0.01
