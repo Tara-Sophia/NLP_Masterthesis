28938352fc3d88e92d14be2e8df79b1afcc282da
Florentin Haugwitz <florentin.vonhaugwitz@gmail.com>
2022-11-03 21:44:21 +0000

    Remove checkpoint

--- src/stt/wav2vec2_training.py
+++ src/stt/wav2vec2_training.py
@@ -164,7 +164,7 @@ def main():
     else:
         last_checkpoint = None
 
-    trainer.train(resume_from_checkpoint=last_checkpoint)
+    trainer.train(resume_from_checkpoint=None)
     # processor.save_pretrained(WAV2VEC2_MODEL_DIR)
     # trainer.save_model(WAV2VEC2_MODEL_DIR)
     # trainer.save_state()
