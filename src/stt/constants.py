2bec7326829e6583cfdec474bfc51982d4083954
Florentin Haugwitz <florentin.vonhaugwitz@gmail.com>
2022-11-04 13:29:43 +0000

    More procs

--- src/stt/constants.py
+++ src/stt/constants.py
@@ -13,7 +13,7 @@ RAW_RECORDINGS_DIR = os.path.join(RAW_DATA_DIR, "recordings")
 CHARS_TO_IGNORE_REGEX = '[\,\?\.\!\-\;\:"\[\]]'
 SAMPLING_RATE = 16000
 MAX_DURATION_LENGTH = 10
-NUM_PROC = 1
+NUM_PROC = 7
 PROCESSED_DIR = os.path.join("data", "processed", "stt")
 VOCAB_DIR = os.path.join("models", "stt", "vocab")
 
