b3a29f34f0afba58c5b12aa5861b3a80d9ced5f9
HannahPetry <48458@novasbe.pt>
2022-11-08 14:24:22 +0000

    add conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -8,15 +8,15 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..", "src", "clf")))
+
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Masterthesis"
 copyright = "2022, Hannha Petry, Tara-Sophia Tumbraegel, Florentin von Haugwitz"
-author = (
-    "Hannha Petry, Tara-Sophia Tumbraegel, Florentin von Haugwitz"
-)
+author = "Hannha Petry, Tara-Sophia Tumbraegel, Florentin von Haugwitz"
 release = "0.1"
 
 # -- General configuration ---------------------------------------------------
