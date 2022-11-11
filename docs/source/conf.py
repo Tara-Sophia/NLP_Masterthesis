fc306d5ec9a51f602114a1287ee1fc73b6935459
Florentin Haugwitz <florentin.vonhaugwitz@gmail.com>
2022-11-11 21:17:57 +0000

    Adding config files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,16 +11,16 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
-sys.path.insert(1, os.path.abspath(os.path.join("..", "..", "src")))
-sys.path.insert(
-    2, os.path.abspath(os.path.join("..", "..", "src", "data"))
-)
-sys.path.insert(
-    3, os.path.abspath(os.path.join("..", "..", "src", "clf"))
-)
-sys.path.insert(
-    4, os.path.abspath(os.path.join("..", "..", "src", "stt"))
-)
+# sys.path.insert(1, os.path.abspath(os.path.join("..", "..", "src")))
+# sys.path.insert(
+#     2, os.path.abspath(os.path.join("..", "..", "src", "data"))
+# )
+# sys.path.insert(
+#     3, os.path.abspath(os.path.join("..", "..", "src", "clf"))
+# )
+# sys.path.insert(
+#     4, os.path.abspath(os.path.join("..", "..", "src", "stt"))
+# )
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
