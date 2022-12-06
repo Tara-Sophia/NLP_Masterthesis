f816ec0ec40f88a403fba0da7a77b3d7b1cbfe87
HannahPetry <48458@novasbe.pt>
2022-12-06 11:38:32 +0000

    add models

--- src/clf/constants.py
+++ src/clf/constants.py
@@ -28,14 +28,15 @@ RAW_DATA_DIR_MT_MLM = os.path.join(
 RAW_DATA_DIR_MIMIC = os.path.join(
     "data",
     "processed",
-    "nlp",
-    "mtsamples",
+    "mimic_iii",
     "mimic_TC.csv",
 )
 
 # columns
 X_MASKED = "transcription_f_MLM"
 X_CLASSIFIED = "transcription_f_TC"
+# columns and labels is specialty not medical_specialty
+X_MIMIC = "healthrecord_f_TC"
 
 # path to train data
 TRAIN_DATA_DIR = os.path.join("data", "processed", "clf", "train.csv")
@@ -47,6 +48,7 @@ TEST_DATA_DIR = os.path.join("data", "processed", "clf", "test.csv")
 LR_MT_MASKED = os.path.join("models", "clf", "lr_mt_masked.pkl")
 LR_MT_CLASSIFIED = os.path.join("models", "clf", "lr_mt_classified.pkl")
 LR_MIMIC_CLASSIFIED = os.path.join("models", "clf", "lr_mimic_classified.pkl")
+LR_MIMIC_TEST = os.path.join("models", "clf", "lr_mimic_test.pkl")
 
 RF_MT_MASKED = os.path.join("models", "clf", "rf_mt_masked.pkl")
 RF_MT_CLASSIFIED = os.path.join("models", "clf", "rf_mt_classified.pkl")
@@ -55,10 +57,12 @@ RF_MIMIC_CLASSIFIED = os.path.join("models", "clf", "rf_mimic_classified.pkl")
 DT_MT_MASKED = os.path.join("models", "clf", "dt_mt_masked.pkl")
 DT_MT_CLASSIFIED = os.path.join("models", "clf", "dt_mt_classified.pkl")
 DT_MIMIC_CLASSIFIED = os.path.join("models", "clf", "dt_mimic_classified.pkl")
+DT_MIMIC_TEST = os.path.join("models", "clf", "dt_mimic_test.pkl")
 
 SVM_MT_MASKED = os.path.join("models", "clf", "svm_mt_masked.pkl")
 SVM_MT_CLASSIFIED = os.path.join("models", "clf", "svm_mt_classified.pkl")
 SVM_MIMIC_CLASSIFIED = os.path.join("models", "clf", "svm_mimic_classified.pkl")
+SVM_MIMIC_TEST = os.path.join("models", "clf", "svm_mimic_test.pkl")
 
 XGB_MT_MASKED = os.path.join("models", "clf", "xgb_mt_masked.pkl")
 XGB_MT_CLASSIFIED = os.path.join("models", "clf", "xgb_mt_classified.pkl")
